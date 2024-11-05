f7fa62e15e1201852ecc780331ee7e83742b0d38
Anna Kislenkova <anna.kislenkova@icloud.com>
2024-11-05 02:38:56 +0000

    code

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,21 @@
 // import statements
-
+import java.util.Scanner;
+import java.util.PriorityQueue;
+import java.lang.Integer;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class PriorityQueueLab {
-  
+  public static void main(String[] args) throws FileNotFoundException{
+    File f = new File("/workspaces/priority-queue-lab-akislenkova/RandIntegers.txt");
+    Scanner scan = new Scanner(f);
+    PriorityQueue<Integer> queue = new PriorityQueue<>();
+    while(scan.hasNextLine()){
+      String a = scan.nextLine();
+      int b = Integer.parseInt(a);
+      queue.add(b);
+    }
+    System.out.println(queue);
+    scan.close();
+  }
 }
 
